a46f14d463b77ba57524f51ca03e410879975e51
Mark Ritchie <markjritchie@hotmail.com>
2018-12-12 09:11:40 +0000

    Initial work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChange } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Location } from './models/location';
+import { MatSelectChange } from '@angular/material';
+
+export interface CityValue {
+  value: Location;
+  text: string;
+}
+
+export interface Countries {
+  disabled?: boolean;
+  name: string;
+  cities: CityValue[];
+}
 
 @Component({
   selector: 'mjr-root',
@@ -6,5 +20,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mjr';
+  title = `Mark's very underwhelming weather site.`;
+  location: Location;
+  cityControl = new FormControl();
+  countries: Countries[] = [
+    {
+      name: 'UK',
+      cities: [
+        { value: { city: 'Edinburgh', country: 'uk' }, text: 'Edinburgh' },
+        { value: { city: 'Glasgow', country: 'uk' }, text: 'Glasgow' },
+        { value: { city: 'East Kilbride', country: 'uk' }, text: 'East Kilbride' }
+      ]
+    },
+    {
+      name: 'US',
+      cities: [
+        { value: { city: 'Los Angeles', country: 'us'}, text: 'LA' },
+        { value: { city: 'New York', country: 'us'}, text: 'New York' },
+        { value: { city: 'Miami', country: 'us'}, text: 'Miami' }
+      ]
+    },
+    {
+      name: 'Ooops',
+      cities: [
+        { value: { city: 'Somewhere', country: 'Nowhere'}, text: 'Lets have an error' },
+      ]
+    }
+  ];
+
+  onChange(event: MatSelectChange) {
+    this.location = event.value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WeatherComponent } from './weather/weather.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    ReactiveFormsModule
   ],
+  exports: [MatSnackBarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
